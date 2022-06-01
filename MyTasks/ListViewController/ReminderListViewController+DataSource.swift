39b0b6251bb8b9209fb3c9a34882b6e2139fb822
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-05-31 21:53:11 -0400

    Reminder Date+Time Collection View

---
+++ MyTasks/ListViewController/ReminderListViewController+DataSource.swift
@@ -0,0 +1,42 @@
+//
+//  ReminderListViewController+DataSource.swift
+//  MyTasks
+//
+//  Created by Nicholas McGinnis on 5/30/22.
+//
+
+import UIKit
+
+
+extension ReminderListViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfiguration
+        
+        var doneButtonConfiguration = doneButtonConfiguration(for: reminder)
+        doneButtonConfiguration.tintColor = UIColor(named: "TodayListCellDoneButtonTint")
+        cell.accessories = [ .customView(configuration: doneButtonConfiguration), .disclosureIndicator(displayed: .always)]
+        
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+        backgroundConfiguration.backgroundColor = UIColor(named: "TodayListCellBackground")
+        cell.backgroundConfiguration = backgroundConfiguration
+    }
+    
+    private func doneButtonConfiguration(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration {
+        let symbolName = reminder.isComplete ? "circle.fill" : "circle"
+        let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
+        let image = UIImage(systemName: symbolName, withConfiguration: symbolConfiguration)
+        let halfImage = UIImage(systemName: "scribble.variable")
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        button.setImage(halfImage, for: .highlighted)
+        return UICellAccessory.CustomViewConfiguration(customView: button, placement: .leading(displayed: .always))
+    }
+}

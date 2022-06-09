410e5f2c0b745b0b018d993236b17965e786e191
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-06-08 22:55:49 -0400

    Created sections for an editing mode

--- MyTasks/AppDelegate.swift
+++ MyTasks/AppDelegate.swift
@@ -14,6 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UINavigationBar.appearance().tintColor = UIColor(named: "TodayPrimaryTint")
+        UINavigationBar.appearance().backgroundColor = UIColor(named: "TodayNavigationBackground")
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
         return true
     }
 
---
+++ MyTasks/DetailViewController/ReminderViewController+Section.swift
@@ -0,0 +1,30 @@
+//
+//  ReminderViewController+Section.swift
+//  MyTasks
+//
+//  Created by Nicholas McGinnis on 6/8/22.
+//
+
+import Foundation
+
+extension ReminderViewController {
+    enum Section: Int, Hashable {
+        case view
+        case title
+        case date
+        case notes
+        
+        var name: String {
+            switch self {
+            case .view:
+                return ""
+            case .title:
+                return NSLocalizedString("Title", comment: "Title section name")
+            case .date:
+                return NSLocalizedString("Date", comment: "Date section name")
+            case .notes:
+                return NSLocalizedString("Notes", comment: "Notes section name")
+            }
+        }
+    }
+}

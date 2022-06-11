04c92850aba458aea44cd71190590ada44a599f8
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-06-10 20:43:02 -0400

    Conformed textfield content view protocol

---
+++ MyTasks/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  MyTasks
+//
+//  Created by Nicholas McGinnis on 6/10/22.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
